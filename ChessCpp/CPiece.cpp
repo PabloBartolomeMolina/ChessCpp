9e6f6b15895d9ab3fe534890b760782abddcf923
Pablo Bartolomé <pablobart.me@gmail.com>
2021-05-12 22:02:35 +0200

    Creation of class for pieces

---
+++ ChessCpp/CPiece.cpp
@@ -0,0 +1,19 @@
+#include "CPiece.h"
+
+/* Basic methods, like constructor and destructor. */
+
+/// <summary>
+/// Public constructor to set up basic elements of the Board objects.
+/// </summary>
+CPiece::CPiece()
+{
+
+}
+
+/// <summary>
+/// Public destructor.
+/// </summary>
+CPiece::~CPiece()
+{
+
+}
---
+++ ChessCpp/CPiece.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <iostream>
+#include <map>
+#include "Defines.h"
+
+using namespace std;
+
+class CPiece
+{
+private:
+	map<string, string> piece = { "NaN", "Nan" };
+public:
+	
+	CPiece();
+	~CPiece();
+};
+
