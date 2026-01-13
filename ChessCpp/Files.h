e99d71a746d05e6b1bc799631cd28cc84aa25373
Pablo Bartolomé <pablobart.me@gmail.com>
2026-01-13 22:22:35 +0100

    Remove old version to rewrite the project in a proper way

--- ChessCpp/Files.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <fstream>
-#include <iostream>
-#include <string>
-
-bool fileWrite(std::string newLine);
-bool fileRead(std::string info);
--- ChessCpp/Movement.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <conio.h>
-#include <iostream>
-#include <vector>
-
-std::string * Movement();
-bool check_input(std::string &move);
